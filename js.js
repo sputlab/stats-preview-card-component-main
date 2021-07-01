f4b815fef55fda853c790e9ee03b8ecbf782f778
Sputnik <84189179+sputlab@users.noreply.github.com>
2021-07-01 13:41:01 +0200

    Update js.js

--- js.js
+++ js.js
@@ -8,6 +8,8 @@ function getMilk(bottles) {
   console.log("moveUp");
   console.log("moveRight");
   console.log("moveRight");
+  var dollars = prompt("How many dollars you have?");
+  var bottles = dollars % 3;
   console.log("Buy " + bottles + " bottles of milk!")
   console.log("moveLeft");
   console.log("moveLeft");
